a25e25fe7b0cf385a6f7425c5f79a0ed5dff68bf
chanzb0119 <xiaophai2001@gmail.com>
2023-09-01 20:37:29 +0800

    Processing genre page

---
+++ scripts/products.js
@@ -0,0 +1,47 @@
+export const products = [
+    {
+        id:1,
+        image:"..\images\product\the-women-of-chateau-lafayette.jpg",
+        title:"The Women of Chateau Lafayette",
+        author:"Stephanie Dray",
+        rental: 5,
+        deposit: 50,
+        summary: "An epic saga from New York Times bestselling author Stephanie Dray based on the true story of an extraordinary castle in the heart of France and the remarkable women bound by its legacy."
+    },
+    {
+        id:2,
+        image:"..\images\product\one-hundred-years-of-lenni-and-margot.jpg",
+        title:"One Hundred Years of Lenni and Margot",
+        author:"Marianne Cronin",
+        rental: 5,
+        deposit: 50,
+        summary: "Life is short. No-one knows that better than seventeen year old Lenni living on the terminal ward. But as she is about to learn, it’s not only what you make of life that matters, but who you share it with."
+    },
+    {
+        id:3,
+        image:"..\images\product\the-lord-of-the-rings.jpg",
+        title:"The Lord of the Rings",
+        author:"J.R.R. Tolkien,Ian Holm,John Le Mesurier,Michael Hordern,Peter Woodthorpe,Robert Stephens",
+        rental: 5,
+        deposit: 50,
+        summary:"All three parts of the epic masterpiece The Lord of the Rings – The Fellowship of the Ring, The Two Towers & The Return of the King – available as one download, featuring the definitive edition of the text, hyperlinked footnotes and page references, and 3 maps including a detailed map of Middle-earth."
+    },
+    {
+        id:4,
+        image:"..\images\product\the-man-who-died-twice.jpg",
+        title:"The Man Who Died Twice",
+        author:"Richard Osman",
+        rental: 5,
+        deposit: 50,
+        summary:"“It’s the following Thursday. Elizabeth has received a letter from an old colleague, a man with whom she has a long history. He’s made a big mistake, and he needs her help. His story involves stolen diamonds, a violent mobster and a very real threat to his life. As bodies start piling up, Elizabeth enlists Joyce, Ibrahim and Ron in the hunt for a ruthless murderer. And if they find the diamonds too? Well, wouldn’t that be a bonus? But this time they are up against an enemy who wouldn’t bat an eyelid at knocking off four septuagenarians. Can The Thursday Murder Club find the killer (and the diamonds) before the killer finds them?”–Publisher."
+    },
+    {
+        id:5,
+        image:"..\images\product\the-paris-library.jpg",
+        title:"The Paris Library",
+        author:"Janet Skeslien Charles",
+        rental: 5,
+        deposit: 50,
+        summary:"Based on the true World War II story of the heroic librarians at the American Library in Paris, this is an unforgettable story of romance, friendship, family, and the power of literature to bring us together, perfect for fans of The Lilac Girls and The Paris Wife."
+    }
+];
