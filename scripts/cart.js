64897450e59e0d3ba4b3d0e133d8ed2693c66c5f
Dyunamisu <cleveralvin08101@gmail.com>
2023-09-11 21:02:12 +0800

    update wishList

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,5 +1,13 @@
 $(function(){
-    for(let i=0;i<3;i++){
-        
-    }
+    let storeId = 2;
+    products.forEach(product =>{
+        if(product.id === storeId){
+            $(".cart-name").append(`<p>${product.title}</p>`);
+            $(".cart-price").append(`<p>RM ${product.deposit}</p>`);
+            let q = 2;
+            $(".cart-quantity").append(`<p>${q}</p>`);
+            subTotal = q * product.deposit;
+            $(".cart-subtotal").append(`<p>RM ${subTotal}</p>`);
+        }
+    });
 })
