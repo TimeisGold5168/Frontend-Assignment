6ab753f2ffcc8d369c98576af7ebce7e0a379b1c
Dyunamisu <cleveralvin08101@gmail.com>
2023-09-12 21:01:41 +0800

    Dynamic Update Quantity in LocalStorage

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,31 +1,30 @@
 $(function(){
 
-    (currentUser.cart).forEach((cartItem) =>{
-        const deposit = parseInt(cartItem.deposit.split(" ")[1]);
-        const rental = parseInt(cartItem.rental.split(" ")[1]);
+    for(let i=0; i<currentUser.cart.length; i++){
+        const deposit = parseInt(currentUser.cart[i].deposit.split(" ")[1]);
+        const rental = parseInt(currentUser.cart[i].rental.split(" ")[1]);
         const sum = deposit + rental;
-        let quantity = cartItem.quantity;
+        let quantity = currentUser.cart[i].quantity;
 
-        $(".cart-name").append(`<p class="my-2">${cartItem.title}</p>`);
+        $(".cart-name").append(`<p class="my-2">${currentUser.cart[i].title}</p>`);
         $(".cart-price").append(`<p class="my-2">RM ${sum}</p>`);
-        $(".cart-quantity").append(`<input type="number" class="form-control quantity my-2" data-id="${cartItem.title}" value="${quantity}" min="1">`);
-        $(".cart-subtotal").append(`<p class="subtotal my-2" data-id="${cartItem.title}">RM ${sum}</p>`);
+        $(".cart-quantity").append(`<input type="number" class="form-control quantity my-2" data-id="${i}" value="${quantity}" min="1">`);
+        $(".cart-subtotal").append(`<p class="subtotal my-2" data-id="${i}">RM ${sum}</p>`);
 
         // handle quantity increase event
-        $(`.quantity[data-id="${cartItem.title}"]`).on('change',()=>{
+        $(`.quantity[data-id="${i}"]`).on('change',()=>{
 
             //dynamically increase subtotal
-            quantity = parseInt($(`.quantity[data-id="${cartItem.title}"]`).val());
+            quantity = parseInt($(`.quantity[data-id="${i}"]`).val());
             const subTotal = quantity * sum;
-            $(`.subtotal[data-id="${cartItem.title}"]`).text(`RM ${subTotal}`);
+            $(`.subtotal[data-id="${i}"]`).text(`RM ${subTotal}`);
 
             //update localStorage quantity
             const user = JSON.parse(localStorage.getItem(username)); // Retrieve the user object
-            user.cart.forEach((user)=>{
-                if(cartItem.title=)
-            })
+            user.cart[i].quantity = quantity;
+            localStorage.setItem(username,JSON.stringify(user));
         })
-    });
+    };
 
     (currentUser.wishlist).forEach((wishListItem) =>{
         const title = wishListItem.title
@@ -36,15 +35,4 @@ $(function(){
         $(".wishList-name").append(`<p class="my-2">${title}</p>`);
         $(".wishList-price").append(`<p class="my-2">RM ${sum}</p>`);
     });
-
-    const user = JSON.parse(localStorage.getItem(username)); // Retrieve the user object
-    user.cart[0].quantity = 1;
-    localStorage.setItem(username, JSON.stringify(user));
-    console.log(user);
-    // if (user && user.cart && user.cart[itemIndex]) {
-    // user.cart[itemIndex].quantity = updatedQuantity;
-    
-    // // Step 3: Update the entire object in localStorage
-    // localStorage.setItem('user', JSON.stringify(user));
-    //}
 })
