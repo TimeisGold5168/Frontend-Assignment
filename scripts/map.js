1974dda78c2f948a1b11fe76a8b4a6450f15bfbc
Baldingisdestiny <zhenting5168@gmail.com>
2023-09-16 01:03:09 +0800

    restful api added

--- scripts/map.js
+++ scripts/map.js
@@ -20,3 +20,56 @@ async function initMap() {
 }
 
 initMap();
+
+$('.location-button').click(function () {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log(position);
+      var apiKey = "AIzaSyBi-h4GZDj6PFwsslaxjgoqvJqlHDNVzyA"; // Replace with your API key
+      $.get(
+        "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+        position.coords.latitude +
+        "," +
+        position.coords.longitude +
+        "&sensor=false&key=" +
+        apiKey, // Include your API key here
+        function (data) {
+          console.log(data);
+        }
+      );
+      var img = document.getElementById("map-image");
+      var latitude = position.coords.latitude;
+      var longitude = position.coords.longitude;
+      var zoomLevel = 15; // Set the desired zoom level
+
+      // Set the marker icon and label (you can customize these)
+      var markerIcon = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"; // URL to a marker icon
+      var markerLabel = "Marker"; // Label for the marker
+
+      img.src =
+        "https://maps.googleapis.com/maps/api/staticmap?center=" +
+        latitude +
+        "," +
+        longitude +
+        "&zoom=" + zoomLevel + // Set the zoom level here
+        "&size=800x400&markers=label:" +
+        markerLabel +
+        "%7Cicon:" +
+        markerIcon +
+        "%7C" +
+        latitude +
+        "," +
+        longitude +
+        "&sensor=false&key=" +
+        apiKey; // Include your API key here
+      // Make the container responsive
+      var container = document.querySelector(".output");
+      container.style.width = "100%";
+      container.style.maxWidth = "auto"; // Adjust the maximum width as needed
+
+      // Make the image responsive
+      img.style.width = "100%";
+      img.classList.remove("hidden");
+    });
+  }
+});
