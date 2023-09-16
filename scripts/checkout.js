4fbc0280745f88666654d78b0555d3d7d8a60abe
Baldingisdestiny <zhenting5168@gmail.com>
2023-09-16 21:28:39 +0800

    checkout backend done, added user.payment field

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -21,3 +21,65 @@ $(function(){
 
     
 })
+
+function handleSaveAddressCheckbox() {
+    const checkbox = $("#SaveAddress");
+    if (!currentUser.address) {
+        currentUser.address = {};
+      }
+
+    if (checkbox.is(":checked")) {
+  
+  
+      currentUser.address.firstName =$("#Fname").val();
+      currentUser.address.lastName =$("#Lname").val();
+      currentUser.address.addressLine1 =$("#Address1").val();
+      currentUser.address.addressLine2 =$("#Address2").val();
+      currentUser.address.city = $("#City").val();
+      currentUser.address.state = $("#State").val();
+      currentUser.address.postalCode = $("#Zip").val();
+      currentUser.address.phoneNumber = $("#Phone").val();
+  
+      
+      alert("Billing Address saved successfully")
+      localStorage.setItem(currentUser.username, JSON.stringify(currentUser));
+    }
+  }
+  
+  
+  $("#SaveAddress").on("change", handleSaveAddressCheckbox);
+
+  $(document).ready(function() {
+    // Add a submit event listener to the form
+    $("#paymentForm").submit(function(event) {
+      event.preventDefault(); // Prevent the form from actually submitting
+  
+      // Get the values from the form fields
+      const cardname = $("#Creditname").val();
+      const cardNo = $("#CreditNo").val();
+      const expMth = $("#Expmonth").val();
+      const expYr = $("#Expyear").val();
+      const CVV = $("#CVV").val();
+  
+      // Create an object with the payment information
+      const paymentInfo = {
+        cardname,
+        cardNo,
+        expMth,
+        expYr,
+        CVV
+      };
+  
+      // Store the payment information in session storage
+      sessionStorage.setItem("currentUser.payment", JSON.stringify(paymentInfo));
+  
+      // Inform the user that the payment information has been saved
+      alert("Payment information has been saved.");
+  
+      // Clear session storage when the tab is closed
+      $(window).on("beforeunload", function() {
+        sessionStorage.removeItem("currentUser.payment");
+      });
+      window.location.href = "genre.html";
+    });
+  });
