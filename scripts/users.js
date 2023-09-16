0c1cc72c1dc523b9cb51ca3055b72adf303dc343
Vicky <vickyalem.01@gmail.com>
2023-09-16 19:10:57 +0800

    update users.js

--- scripts/users.js
+++ scripts/users.js
@@ -1,24 +1,46 @@
 let defaultUsers = [{
-    username: "user123", 
-    password: "123", 
-    email:"123@123.com",
-    receivedate : "20/10",
+    username: "user123",
+    password: "123",
+    email: "123@123.com",
+    receivedate: "20/10",
     wishlist: [],
-    cart:[] ,
-    },
-    { 
-    username: "user456", 
-    password: "456",
-    email:"456@456.com",
-    wishlist:[],
     cart: [],
-    receivedate : " ",
+    membership: "none",
+    address: {
+        firstName: "",
+        lastName: "",
+        addressLine1: "",
+        addressLine2: "",
+        city: "",
+        state: "",
+        postalCode: "",
+        phoneNumber: "",
     },
+},
+{
+    username: "user456",
+    password: "456",
+    email: "456@456.com",
+    wishlist: [],
+    cart: [],
+    receivedate: " ",
+    membership: "none",
+    address: {
+        firstName: "",
+        lastName: "",
+        addressLine1: "",
+        addressLine2: "",
+        city: "",
+        state: "",
+        postalCode: "",
+        phoneNumber: "",
+    },
+},
 ];
 
 //retrieved users from localstorage
 let users = JSON.parse(localStorage.getItem("users"));
 
-if(users == null) {
+if (users == null) {
     users = defaultUsers;
 }
